dd0268a830edff7e2669e52adb8e139d75f380fb
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-11-11 12:00:02 +0700

    setup routing

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white"></div>
+    </div>
+  );
+};
+
+export default Auth;
