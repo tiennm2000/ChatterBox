5e1745399b592d937e1386a1fc0175a846e9ac9e
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-11-12 19:46:59 +0700

    auth ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Button } from "./components/ui/button";
-import Auth from "./pages/auth";
-import Chat from "./pages/chat";
-import Profile from "./pages/profile";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Button } from './components/ui/button';
+import Auth from './pages/auth';
+import Chat from './pages/chat';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/chat" element={<Chat />} />
         <Route path="/profile" element={<Profile />} />
 
-        <Route path="*" element={<Navigate to={"/auth"} />} />
+        <Route path="*" element={<Navigate to={'/auth'} />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,7 +1,103 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Background from '../../assets/login1.jpg';
+import Victory from '../../assets/victory.svg';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleLogin = async () => {};
+
+  const handleSignup = async () => {};
+
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-white border-2 border-white"></div>
+      <div
+        className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-xl w-[80vw]
+        md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"
+      >
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex items-center justify-center ">
+              <h1 className="text-5xl font-bold md:text-6xl ">Welcome</h1>
+              <img src={Victory} alt="victory image" className="h-[100px]" />
+            </div>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the Chatter Box
+            </p>
+          </div>
+          <div className="flex items-center justify-center w-full ">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  SignUp
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="flex flex-col gap-5 mt-10" value="login">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleLogin}>
+                  Login
+                </Button>
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-5" value="signup">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button className="rounded-full p-6" onClick={handleSignup}>
+                  Signup
+                </Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex items-center justify-center">
+          <img src={Background} alt="background-login" className="h-[500px]" />
+        </div>
+      </div>
     </div>
   );
 };
