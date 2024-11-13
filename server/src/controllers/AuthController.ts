8d41829a6f840543338fa5475332e78587a7f0bb
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-11-13 12:24:25 +0700

    user model

--- server/src/controllers/AuthController.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    require: [true, "Email is required."],
-    unique: true,
-  },
-});
---
+++ server/src/models/UserModel.ts
@@ -0,0 +1,52 @@
+import mongoose, { Document } from "mongoose";
+import { genSalt, hash } from "bcrypt";
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+  image?: string;
+  color?: number;
+  profileSetup: boolean;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  email: {
+    type: String,
+    require: [true, "Email is required."],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "Password is required."],
+  },
+  firstName: {
+    type: String,
+    require: false,
+  },
+  lastName: {
+    type: String,
+    require: false,
+  },
+  image: {
+    type: String,
+    require: false,
+  },
+  color: {
+    type: Number,
+    require: false,
+  },
+  profileSetup: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await genSalt();
+  this.password = await hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.models.Users || mongoose.model("Users", userSchema);
