dd14116e7201afb2ac549ead80d2e91e4a5f6a13
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-02 04:00:53 +0700

    message model

---
+++ server/src/models/MessagesModel.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document } from "mongoose";
+
+interface IMessage extends Document {
+  sender: mongoose.Types.ObjectId;
+  recipient?: mongoose.Types.ObjectId;
+  messageType: "text" | "file";
+  content?: string;
+  fileUrl?: string;
+  timestamp: Date;
+}
+
+const messageSchema = new mongoose.Schema<IMessage>({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: false,
+  },
+  messageType: {
+    type: String,
+    enum: ["text", "file"],
+    required: true,
+  },
+
+  content: {
+    type: String,
+    required: function () {
+      return this.messageType === "text";
+    },
+  },
+  fileUrl: {
+    type: String,
+    required: function () {
+      return this.messageType === "file";
+    },
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Messages", messageSchema);
+
+export default Message;
