2b5c41372517bd819b9aab1a50cbd565f6f0097d
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-02 04:51:16 +0700

    add type Message

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -7,3 +7,13 @@ export interface UserInfo {
   image?: string;
   color?: number;
 }
+
+export interface Message {
+  id: string;
+  sender: UserInfo;
+  recipient?: UserInfo;
+  messageType: 'text' | 'file';
+  content?: string;
+  fileUrl?: string;
+  timestamp: Date;
+}
