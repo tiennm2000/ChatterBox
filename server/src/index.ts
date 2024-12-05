6576bdb44291cd578c215935e1d5aef0495457f3
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-06 02:26:24 +0700

    upload file with static route

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files", express.static("uploads/files"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactsRoutes);
