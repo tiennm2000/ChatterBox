a35122e1de281c42415dca57fed3cdc9a336bbc6
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-06 02:24:38 +0700

    route file

--- server/src/routes/MessageRoute.ts
+++ server/src/routes/MessageRoute.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware";
-import { getMessages } from "../controllers/MessageController";
+import { getMessages, uploadFile } from "../controllers/MessageController";
+import multer from "multer";
 
 const messagesRoutes = Router();
+const upload = multer({ dest: "uploads/files" });
 
 messagesRoutes.post("/get-messages", verifyToken, getMessages);
+messagesRoutes.post(
+  "/upload-file",
+  verifyToken,
+  upload.single("file"),
+  uploadFile
+);
 
 export default messagesRoutes;
