8dfe8d6cd907ca9ee017f2e7a80eee4be3d2439d
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-06 02:28:57 +0700

    display file image and other file on message container

--- client/src/pages/chat/components/chat-container/components/message-container/index.tsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.tsx
@@ -3,7 +3,9 @@ import { useEffect, useRef } from 'react';
 import moment from 'moment';
 import { Message } from '@/utils/types';
 import { apiClient } from '@/lib/api-client';
-import { GET_ALL_MESSAGES_ROUTE } from '@/utils/constants';
+import { GET_ALL_MESSAGES_ROUTE, HOST } from '@/utils/constants';
+import { MdFolderZip } from 'react-icons/md';
+import { IoMdArrowRoundDown } from 'react-icons/io';
 
 const MessageContainer = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -43,8 +45,6 @@ const MessageContainer = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-
-    console.log(selectedChatMessages);
   }, [selectedChatMessages]);
 
   const renderMessages = () => {
@@ -66,6 +66,16 @@ const MessageContainer = () => {
     });
   };
 
+  const checkIfImage = (filePath: string) => {
+    const imageRegex =
+      /\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
+    return imageRegex.test(filePath);
+  };
+
+  const downloadFile = async (fileUrl: string) => {
+    const response = await apiClient.get(`${HOST}/{fileUrl}`);
+  };
+
   const renderDMMessages = (message: Message) => (
     <div
       className={
@@ -83,6 +93,38 @@ const MessageContainer = () => {
           } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
         >
           {message.content}
+        </div>
+      )}
+      {message.messageType === 'file' && (
+        <div
+          className={`${
+            message.sender._id !== selectedChatData?._id
+              ? 'bg-pastel-light-pink border-blue-600/50'
+              : 'bg-pastel-sky-purple border-black/50'
+          } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
+        >
+          {checkIfImage(message.fileUrl!) ? (
+            <div className="cursor-pointer">
+              <img
+                src={`${HOST}/${message.fileUrl}`}
+                height={300}
+                width={300}
+              />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center gap-4">
+              <span className="text-black/20 text-3xl bg-pastel-lavender/20 rounded-full p-3">
+                <MdFolderZip />
+              </span>
+              <span>{message.fileUrl?.split('/').pop()}</span>
+              <span
+                className="bg-black/20 p-3 text-2xl rounded-full cursor-pointer hover:bg-black/50 transition-all duration-300"
+                onClick={() => downloadFile(message.fileUrl)}
+              >
+                <IoMdArrowRoundDown />
+              </span>
+            </div>
+          )}
         </div>
       )}
       <div className="text-xs text-gray-700">
