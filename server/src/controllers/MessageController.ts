c9c4c9c16c63466f7a476c60e216b7e613880ecb
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-06 02:25:44 +0700

    upload file

--- server/src/controllers/MessageController.ts
+++ server/src/controllers/MessageController.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { ExtendedRequest } from "../middlewares/AuthMiddleware";
 import Message from "../models/MessagesModel";
+import { mkdirSync, renameSync } from "fs";
 
 export const getMessages: RequestHandler = async (
   request: ExtendedRequest,
@@ -18,6 +19,30 @@ export const getMessages: RequestHandler = async (
     }).sort({ timestamp: 1 });
 
     response.status(200).json({ messages });
+  } catch (error) {
+    console.error(error);
+    response.status(400).send("Internal Server Error");
+  }
+};
+
+export const uploadFile: RequestHandler = async (
+  request: ExtendedRequest,
+  response
+) => {
+  try {
+    if (!request.file) {
+      response.status(400).send("File is required");
+      return;
+    }
+
+    const date = Date.now();
+    let fileDir = `uploads/files/${date}`;
+    let fileName = `${fileDir}/${request.file.originalname}`;
+
+    mkdirSync(fileDir, { recursive: true });
+    renameSync(request.file.path, fileName);
+
+    response.status(200).json({ filePath: fileName });
   } catch (error) {
     console.error(error);
     response.status(400).send("Internal Server Error");
