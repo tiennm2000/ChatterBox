25718f6b325e3aea0f90dd6a98fc8921b7bef945
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-07 12:07:43 +0700

    add show image when click and download

--- client/src/pages/chat/components/chat-container/components/message-container/index.tsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.tsx
@@ -1,11 +1,12 @@
 import { useAppStore } from '@/store';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import moment from 'moment';
 import { Message } from '@/utils/types';
 import { apiClient } from '@/lib/api-client';
 import { GET_ALL_MESSAGES_ROUTE, HOST } from '@/utils/constants';
 import { MdFolderZip } from 'react-icons/md';
 import { IoMdArrowRoundDown } from 'react-icons/io';
+import { IoCloseSharp } from 'react-icons/io5';
 
 const MessageContainer = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -16,6 +17,9 @@ const MessageContainer = () => {
     selectedChatMessages,
     setSelectedChatMessages,
   } = useAppStore();
+
+  const [showImage, setShowImage] = useState(false);
+  const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
     const getMessages = async () => {
@@ -114,7 +118,13 @@ const MessageContainer = () => {
           } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
         >
           {checkIfImage(message.fileUrl!) ? (
-            <div className="cursor-pointer">
+            <div
+              className="cursor-pointer"
+              onClick={() => {
+                setImageUrl(message.fileUrl!);
+                setShowImage(true);
+              }}
+            >
               <img
                 src={`${HOST}/${message.fileUrl}`}
                 height={300}
@@ -147,6 +157,33 @@ const MessageContainer = () => {
     <div className="flex-1 overflow-y-auto scrollbar-hide p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full ">
       {renderMessages()}
       <div ref={scrollRef} />
+      {showImage && (
+        <div className="fixed z-[1000] top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center backdrop-blur-lg flex-col">
+          <div>
+            <img
+              src={`${HOST}/${imageUrl}`}
+              className="h-[80vh] w-full bg-cover"
+            />
+          </div>
+          <div className="flex gap-5 fixed top-0 mt-5">
+            <button
+              className="bg-black/20 p-3 text-2xl rounded-full cursor-pointer hover:bg-black/50 transition-all duration-300"
+              onClick={() => downloadFile(imageUrl)}
+            >
+              <IoMdArrowRoundDown />
+            </button>
+            <button
+              className="bg-black/20 p-3 text-2xl rounded-full cursor-pointer hover:bg-black/50 transition-all duration-300"
+              onClick={() => {
+                setImageUrl('');
+                setShowImage(false);
+              }}
+            >
+              <IoCloseSharp />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
