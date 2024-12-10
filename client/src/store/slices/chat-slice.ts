b1584d924407c86b83c5fe0eb8d7f9f20d48946e
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-10 19:49:59 +0700

    channel to zustand

--- client/src/store/slices/chat-slice.ts
+++ client/src/store/slices/chat-slice.ts
@@ -1,15 +1,14 @@
-import { Contact, Message, UserInfo } from '@/utils/types';
+import { Channel, Contact, Message, UserInfo } from '@/utils/types';
+
 import { StateCreator } from 'zustand';
 
 export interface ChatSlice {
-  selectedChatType: 'channel' | 'contact' | undefined;
-  selectedChatData: UserInfo | undefined;
+  selectedChatData: UserInfo | Channel | undefined;
   selectedChatMessages: Message[];
   setSelectedChatMessages: (selectedChatMessages: Message[]) => void;
-  setSelectedChatType: (
-    selectedChatType: 'channel' | 'contact' | undefined | undefined
+  setSelectedChatData: (
+    selectedChatData: UserInfo | Channel | undefined
   ) => void;
-  setSelectedChatData: (selectedChatData: UserInfo | undefined) => void;
   closeChat: () => void;
   addMessage: (message: Message) => void;
   directMessageContacts: Contact[];
@@ -22,10 +21,12 @@ export interface ChatSlice {
   setIsDownloading: (isDownloading: boolean) => void;
   setFileUploadProgress: (fileUploadProgress: number) => void;
   setFileDownloadProgress: (fileDownloadProgress: number) => void;
+  channels: Channel[];
+  setChannels: (channels: Channel[]) => void;
+  addChannel: (channel: Channel) => void;
 }
 
 export const createChatSlice: StateCreator<ChatSlice> = (set, get) => ({
-  selectedChatType: undefined,
   selectedChatData: undefined,
   selectedChatMessages: [],
   directMessageContacts: [],
@@ -33,6 +34,8 @@ export const createChatSlice: StateCreator<ChatSlice> = (set, get) => ({
   isDownloading: false,
   fileUploadProgress: 0,
   fileDownloadProgress: 0,
+  channels: [],
+  setChannels: (channels) => set({ channels }),
   setIsUploading(isUploading) {
     set({ isUploading });
   },
@@ -48,24 +51,24 @@ export const createChatSlice: StateCreator<ChatSlice> = (set, get) => ({
   setSelectedChatData(selectedChatData) {
     set({ selectedChatData });
   },
-  setSelectedChatType(selectedChatType) {
-    set({ selectedChatType });
-  },
+
   setSelectedChatMessages: (selectedChatMessages) =>
     set({ selectedChatMessages }),
   setDirectMessageContacts(directMessageContacts) {
     set({ directMessageContacts });
   },
+  addChannel(channel) {
+    const channels = get().channels;
+    set({ channels: [channel, ...channels] });
+  },
   closeChat: () =>
     set({
       selectedChatData: undefined,
-      selectedChatType: undefined,
+
       selectedChatMessages: [],
     }),
   addMessage(message) {
     const selectedChatMessages = get().selectedChatMessages;
-    // const selectedChatType = get().selectedChatType;
-
     set({
       selectedChatMessages: [
         ...selectedChatMessages,
