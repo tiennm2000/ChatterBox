37952985368691c6fccf41c9ed49ffc76a161eeb
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-14 03:34:41 +0700

    send message for channel socket

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -2,6 +2,8 @@ import { Socket, Server as SocketIOServer } from "socket.io";
 import { Server as HTTPServer } from "http";
 import { Server as HTTPSServer } from "https";
 import Message, { IMessage } from "./models/MessagesModel";
+import Channel from "./models/ChannelModel";
+import { MessageChannel } from "./utils/types";
 
 const setupSocket = (server: HTTPSServer | HTTPServer) => {
   const io = new SocketIOServer(server, {
@@ -44,6 +46,43 @@ const setupSocket = (server: HTTPSServer | HTTPServer) => {
     }
   };
 
+  //send message for channel
+  const sendChannelMessage = async (message: MessageChannel) => {
+    const { channelId, sender, content, messageType, fileUrl } = message;
+    const createdMessage = await Message.create({
+      sender,
+      recipient: null,
+      content,
+      messageType,
+      timestamp: new Date(),
+      fileUrl,
+    });
+
+    const messageData = await Message.findById(createdMessage._id)
+      .populate("sender", "_id email firstName lastName image color")
+      .exec();
+
+    await Channel.findByIdAndUpdate(channelId, {
+      $push: { messages: createdMessage._id },
+    });
+
+    const channel = await Channel.findById(channelId).populate("members");
+    const finalData = { ...messageData?.toObject(), channelId };
+
+    if (channel && channel.members) {
+      channel.members.forEach((member) => {
+        const memberSocketId = useSocketMap.get(member._id.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("receive-channel-message", finalData);
+        }
+      });
+      const adminSocketId = useSocketMap.get(channel.admin._id.toString());
+      if (adminSocketId) {
+        io.to(adminSocketId).emit("receive-channel-message", finalData);
+      }
+    }
+  };
+
   //connection
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
@@ -55,6 +94,7 @@ const setupSocket = (server: HTTPSServer | HTTPServer) => {
       console.log("User ID not provide during connection.");
     }
     socket.on("sendMessage", sendMessage);
+    socket.on("send-channel-message", sendChannelMessage);
     socket.on("disconnect", () => disconnect(socket));
   });
 };
