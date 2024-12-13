ce360cac00d10c1f887308e45ee3e24be976c540
tiennm2000 <tien.nm.2409@apteachlearning.edu.vn>
2024-12-14 03:41:31 +0700

    render chat on channel and fetch all messages channel

--- client/src/pages/chat/components/chat-container/components/message-container/index.tsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.tsx
@@ -1,34 +1,46 @@
 import { useAppStore } from '@/store';
 import { useEffect, useRef, useState } from 'react';
 import moment from 'moment';
-import { Message, UserInfo } from '@/utils/types';
+import { Message } from '@/utils/types';
 import { apiClient } from '@/lib/api-client';
-import { GET_ALL_MESSAGES_ROUTE, HOST } from '@/utils/constants';
+import {
+  GET_ALL_MESSAGES_ROUTE,
+  GET_CHANNEL_MESSAGES,
+  HOST,
+} from '@/utils/constants';
 import { MdFolderZip } from 'react-icons/md';
 import { IoMdArrowRoundDown } from 'react-icons/io';
 import { IoCloseSharp } from 'react-icons/io5';
+import { getColor } from '@/lib/utils';
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
+import { AvatarFallback } from '@/components/ui/avatar';
 
 const MessageContainer = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   let {
+    userInfo,
     selectedChatData,
     selectedChatMessages,
     setSelectedChatMessages,
     setIsDownloading,
     setFileDownloadProgress,
-  } = useAppStore();
+    selectedChatType,
+    selectedChatChannel,
+  } = useAppStore.getState();
 
+  console.log(selectedChatMessages);
   const [showImage, setShowImage] = useState(false);
   const [imageUrl, setImageUrl] = useState('');
-  selectedChatData = selectedChatData as UserInfo;
 
   useEffect(() => {
     const getMessages = async () => {
       try {
         const response = await apiClient.post(
           GET_ALL_MESSAGES_ROUTE,
-          { id: selectedChatData._id },
+          {
+            id: selectedChatData?._id,
+          },
           { withCredentials: true }
         );
 
@@ -40,10 +52,29 @@ const MessageContainer = () => {
       }
     };
 
-    if (selectedChatData._id) {
+    const getChannelMessages = async () => {
+      try {
+        const response = await apiClient.get(
+          `${GET_CHANNEL_MESSAGES}/${selectedChatChannel?._id}`,
+          {
+            withCredentials: true,
+          }
+        );
+        if (response.data.messages) {
+          setSelectedChatMessages(response.data.messages);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (selectedChatData?._id && selectedChatType === 'contact') {
       getMessages();
     }
-  }, [selectedChatData, setSelectedChatMessages]);
+    if (selectedChatChannel?._id && selectedChatType === 'channel') {
+      getChannelMessages();
+    }
+  }, [selectedChatData, setSelectedChatMessages, selectedChatChannel]);
 
   useEffect(() => {
     if (scrollRef.current) {
@@ -54,6 +85,7 @@ const MessageContainer = () => {
   const renderMessages = () => {
     let lastDate: null | string = null;
     return selectedChatMessages.map((message) => {
+      console.log(message);
       const messageDate = moment(message.timestamp).format('YYYY-MM-DD');
       const showDate = messageDate !== lastDate;
       lastDate = messageDate;
@@ -64,7 +96,8 @@ const MessageContainer = () => {
               {moment(message.timestamp).format('LL')}
             </div>
           )}
-          {renderDMMessages(message)}
+          {selectedChatType === 'contact' && renderDMMessages(message)}
+          {selectedChatType === 'channel' && renderChannelMessages(message)}
         </div>
       );
     });
@@ -102,13 +135,15 @@ const MessageContainer = () => {
   const renderDMMessages = (message: Message) => (
     <div
       className={
-        message.sender._id === selectedChatData._id ? 'text-left' : 'text-right'
+        message.sender._id === selectedChatData!._id
+          ? 'text-left'
+          : 'text-right'
       }
     >
       {message.messageType === 'text' && (
         <div
           className={`${
-            message.sender._id !== selectedChatData._id
+            message.sender._id !== selectedChatData?._id
               ? 'bg-pastel-light-pink border-blue-600/50'
               : 'bg-pastel-sky-purple border-black/50'
           } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
@@ -119,7 +154,7 @@ const MessageContainer = () => {
       {message.messageType === 'file' && (
         <div
           className={`${
-            message.sender._id !== selectedChatData._id
+            message.sender._id !== selectedChatData?._id
               ? 'bg-pastel-light-pink border-blue-600/50'
               : 'bg-pastel-sky-purple border-black/50'
           } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
@@ -159,6 +194,93 @@ const MessageContainer = () => {
       </div>
     </div>
   );
+
+  const renderChannelMessages = (message: Message) => {
+    return (
+      <div
+        className={`mt-5 ${message.sender._id !== userInfo?._id ? 'text-left' : 'text-right'}`}
+      >
+        {message.messageType === 'text' && (
+          <div
+            className={`${
+              message.sender._id !== userInfo?._id
+                ? 'bg-pastel-light-pink border-blue-600/50'
+                : 'bg-pastel-sky-purple border-black/50'
+            } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
+          >
+            {message.content}
+          </div>
+        )}
+        {message.messageType === 'file' && (
+          <div
+            className={`${
+              message.sender._id !== userInfo?._id
+                ? 'bg-pastel-light-pink border-blue-600/50'
+                : 'bg-pastel-sky-purple border-black/50'
+            } border inline-block p-4 rounded-xl my-1 max-w-[50%] break-words`}
+          >
+            {checkIfImage(message.fileUrl!) ? (
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  setImageUrl(message.fileUrl!);
+                  setShowImage(true);
+                }}
+              >
+                <img
+                  src={`${HOST}/${message.fileUrl}`}
+                  height={300}
+                  width={300}
+                />
+              </div>
+            ) : (
+              <div className="flex items-center justify-center gap-4">
+                <span className="text-black/20 text-3xl bg-pastel-lavender/20 rounded-full p-3">
+                  <MdFolderZip />
+                </span>
+                <span>{message.fileUrl?.split('/').pop()}</span>
+                <span
+                  className="bg-black/20 p-3 text-2xl rounded-full cursor-pointer hover:bg-black/50 transition-all duration-300"
+                  onClick={() => downloadFile(message.fileUrl!)}
+                >
+                  <IoMdArrowRoundDown />
+                </span>
+              </div>
+            )}
+          </div>
+        )}
+        {message.sender._id !== userInfo?._id ? (
+          <div className="flex items-center justify-start gap-3 ">
+            <Avatar className="h-8 w-8 rounded-full overflow-hidden">
+              {message.sender?.image && (
+                <AvatarImage
+                  src={`${HOST}/${message.sender?.image}`}
+                  alt="profile"
+                  className="object-cover h-full w-full bg-pastel-sky-purple"
+                />
+              )}
+
+              <AvatarFallback
+                className={`uppercase h-8 w-8 text-lg  flex items-center justify-center rounded-full ${getColor(message.sender?.color!)}`}
+              >
+                {message.sender?.firstName
+                  ? message.sender?.firstName?.split('').shift()
+                  : message.sender?.email.split('').shift()}
+              </AvatarFallback>
+            </Avatar>
+            <span className="text-sm text-white/60">{`${message.sender.firstName} ${message.sender.lastName}`}</span>
+            <span className="text-xs text-white/60">
+              {moment(message.timestamp).format('LT')}
+            </span>
+          </div>
+        ) : (
+          <div className="text-xs text-white/60 mt-1">
+            {moment(message.timestamp).format('LT')}
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="flex-1 overflow-y-auto scrollbar-hide p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full ">
